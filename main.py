0983ca390d2da5b8cb619c9beb77fa1baf4e8820
Karn <karnssk123@gmail.com>
2023-09-13 00:13:56 +0700

    new code

--- main.py
+++ main.py
@@ -1,83 +1,95 @@
-import itertools
+import itertools # for permutation
 
-# function
+################### functions  #############################
 def sum_(a,b):
-    return a+b
+    if a == None or b == None:
+        return None
+    else:
+        return a+b
+    
 def minus(a,b):
     # aware negative
-    if (a-b) < 0:
-        return b-a
+    if a == None or b == None:
+        return None
     else:
         return a-b
-    #return a-b
+
 def multi(a,b):
-    return a*b
-def divided(a,b):
-    return a/b
-
-func_list = [sum_, minus, multi, divided]
-func_sign = ["+","-","*","/"]
-
-# Start     
-while (True) :   
-    try:
-        user = int(input("Enter 4 numbers >> "))
-        # user = 3737
-        len(str(user)) == 4
-    except Exception:
-        print("Not a Number")
+    if a == None or b == None:
+        return None
     else:
-        break
+        return a*b
 
-# num variable
-firstnum = user // 1000
-secondnum = (user // 100) - (firstnum * 10)
-thirdnum = (user // 10) - (firstnum * 100) - (secondnum * 10)
-forthnum = user - (firstnum * 1000) - (secondnum * 100) - (thirdnum * 10)
+def divided(a,b):
+    if (b == 0 and a == 0) or a == None or b == None:
+        return None
+    elif a == 0:
+        return a/b
+    elif b == 0:
+        return b/a
+    else:
+        return a/b
 
-user_list = [firstnum, secondnum, thirdnum, forthnum]
-
+def aware_negative(num1, sign, num2):
+    global text
+    if num1 == None:
+        return None
+    elif num1 >= num2:
+        text = f"({text}{sign}{num2})"
+    else:
+        text = f"({num2}{sign}{text})"
+        
 def loop_func():
     #  looping user list number
-    global result
-    text = ""
-    user_permutations = list(itertools.permutations(user_list,4))
-    for li in range(24): # permutaions possability
+    global result, user
+    global text
+    user_permutations = list(itertools.permutations(user,4))
+    for li in range(24): # permutaions possability 4*4*4*4
         user_permu_list = list(user_permutations[li])
-        for n in range(4): #  pushing number
-            # operator loop
-            for i in range(4):
-                for j in range(4):
-                    for k in range(4):
 
-                        firstResult = func_list[i](user_permu_list[0], user_permu_list[1])
-                        text = f"({user_permu_list[0]}{func_sign[i]}{user_permu_list[1]})"
-                        secondResult = func_list[j](firstResult, user_permu_list[2])
-                        # aware negative
-                        if firstResult >= user_permu_list[2]:
-                            text = f"({text}{func_sign[j]}{user_permu_list[2]})"
-                        else:
-                            text = f"({user_permu_list[2]}{func_sign[j]}{text})"     
+        # operator loop
+        for i in range(4):
+            for j in range(4):
+                for k in range(4):
 
-                        thirdResult = func_list[k](secondResult, user_permu_list[3])
-                        # aware negative
-                        if secondResult >= user_permu_list[3]:
-                            text = f"{text}{func_sign[k]}{user_permu_list[3]}"
-                        else:
-                            text = f"{user_permu_list[3]}{func_sign[k]}{text}"   
-                            
-                        result = thirdResult
-                        print(text, "", result)
-                        if result == 24:
-                            return text
-            #  pushing number front-back
-            user_permu_list.append(user_permu_list[0])
-            user_permu_list.pop(0)
-    
+                    firstResult = func_list[i](user_permu_list[0], user_permu_list[1]) # first pair operand
+                    text = f"({user_permu_list[0]}{func_sign[i]}{user_permu_list[1]})" # display solution 
+                    secondResult = func_list[j](firstResult, user_permu_list[2])   # second pair operand 
+                    # aware negative and None
+                    aware_negative(firstResult, func_sign[j], user_permu_list[2])    
+                    thirdResult = func_list[k](secondResult, user_permu_list[3]) # third pair operand
+                    # aware negative
+                    aware_negative(secondResult, func_sign[k], user_permu_list[3])
+                        
+                    result = thirdResult # final result from solution
+                    print(text, "", result)
+                    if result == 24:
+                        return text
+################################################################
 
-prove = loop_func()
+text = ""
+func_list = [sum_, minus, multi, divided] # list of function names
+func_sign = ["+","-","*","/"]   # list of operator signs
+# To display the solutions
 
+# Start the program
+while (True) : # user input awaring
+    try:
+        user = [int(n) for n in input("Enter 4 numbers with space: ").split()]
+    except ValueError:
+        print("Not a Number.")
+    else:
+        if len(user) != 4:
+            print("Enter only 4 numbers.")
+        else:
+            break 
+
+prove = loop_func() # calling function
+
+# checking the result returning
 if prove == None:
     print("There is no answer.")
 else:
     print(f"The prove is {prove} = {result}.")
+    
+# program end
